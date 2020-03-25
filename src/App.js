b2001cd14b7cd453509a2f4524a57fdb23a04210
guillaume.pivette <guillaume.pivette@amac02c8690md6r.home>
2020-03-25 16:04:18 +0100

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [pageNumber, setPage] = useState(1)
+
+
+  useEffect(() => {
+
+    fetch('https://api.punkapi.com/v2/beers?page='+pageNumber)
+      .then(response => response.json())
+      .then(setBeers);
+  }, [pageNumber]);
+
+  const increment = () => { setPage(pageNumber + 1) }
+  const decrement = () => {if (pageNumber > 1) {setPage(pageNumber - 1)}}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {beers.map(beer => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={increment}>Increment</button>
+      <button type="button" onClick={decrement}>Decrement</button>
     </div>
   );
 }
+
 
 export default App;
